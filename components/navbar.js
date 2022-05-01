1a0d6665526d495e6102e833caf1a6d0751da89b
Hsudeep <haldarsudeep28@gmail.com>
2022-05-01 22:53:02 +0530

    Road to ReAct 1

---
+++ components/navbar.js
@@ -0,0 +1,23 @@
+function navbar(){
+    return `        
+    <div id="left">
+    <img src="https://www.akamai.com/site/fr/images/logo/2018/frank-and-oak-logo.svg" alt="#Frank and Oak">
+    <p><a href="#">Men</a></p>
+    <p><a href="#">Women</a></p>
+    <p><a href="#">Our Story</a></p>
+    </div>
+    <div id="right">
+    <p>
+        <a href="#">Login-Signup</a>
+        </p>
+    <div id="icons">
+        <!-- <div id="search"> -->
+            <i id="searchfn" class="fa-brands fa-searchengin"></i>
+        <!-- </div> -->
+        <i class="fa-solid fa-cart-shopping"></i>
+    </div> 
+    </div>
+`
+}
+
+export {navbar};
---
+++ components/slidedata.js
@@ -0,0 +1,30 @@
+function slidedata(){
+    return `
+    let slidedata = ["https://cdn.shopify.com/s/files/1/0555/5722/6653/files/HP_-_DESKTOP_-_UNISEX_-_CASUAL_SHORTS_-_APRIL_WK4_2880x.jpg?v=1650908550",
+ "https://cdn.shopify.com/s/files/1/0555/5722/6653/files/HP_-_DESKTOP_-_UNISEX_-_NEW_IN_-_APRIL_WK2_2880x.jpg?v=1649440796",
+ "https://cdn.shopify.com/s/files/1/0555/5722/6653/files/HP_-_DESKTOP_-_UNISEX_-_DENIM_-_MAR_WK3_2880x.jpg?v=1646334148",
+ "https://cdn.shopify.com/s/files/1/0555/5722/6653/files/HP-desktop-unisexe_1_d411c696-3b6d-47c4-b6a1-fa56c5279e03_2880x.jpg?v=1645740394",
+]
+
+localStorage.setItem("slideshow", JSON.stringify(slidedata))
+
+let imagesDiv = document.getElementById("slideshow")
+let i=0;
+let imagedata = JSON.parse(localStorage.getItem("slideshow")) || [];
+
+let image = document.createElement("img");
+image.setAttribute("id", "slideshowImage")
+setInterval(function (){
+    if(i===imagedata.length){
+        i=0;
+    }
+    image.src = imagedata[i]
+
+    imagesDiv.append(image)
+    i++;
+}, 4000);
+
+    `
+}
+
+export {slidedata}
